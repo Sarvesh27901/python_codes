893b1733402bc297edfccb7475c125be71c8205c
Sarvesh Pathak <132951076+Sarvesh27901@users.noreply.github.com>
2023-10-13 22:53:30 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-'''Leetcode easy problem-Two sum'''
+'''Leetcode easy problem-Two sum=1'''
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
